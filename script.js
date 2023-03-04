43d9970a3be2aaf93129a7b696daf8ab629a2670
md-jafari <mohammed.jafari1233@gmail.com>
2023-03-04 14:47:15 +0100

    todolist

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let form = document.querySelector('form')
 let input = document.querySelector('#text-input');
 let list = document.querySelector('#list');
@@ -6,39 +5,43 @@ let allButton = document.querySelector('.all-button');
 let activeButton = document.querySelector('.completed-button');
 let submitButton = document.querySelector('#submit-button')
 let actContainer = document.querySelector('#action-container')
+let numberOfItems = document.querySelector('#number-of-items')
 
-let tasks= [];
-
-
-input.addEventListener('keydown', function(event) {
+let tasks = [];
 
 
-    //keyCode 13 refers to the "Enter" key on keyboard.
-    if(event.keyCode === 13 && input.value != "" && input.value != null){
-
-        //Prevent the default value of the event.
-        event.preventDefault();
-
-        let newTask = {
-            text: input.value,
-            completed: false
-        };
+  input.addEventListener('keydown', function(event) {
     
-        tasks.push(newTask);
 
-        createListItem(newTask);
-        showItems(actContainer);
+    // keyCode 13 refers to the "Enter" key on keyboard.
+    if (event.keyCode === 13 && !input.textContent) {
 
+      event.preventDefault();
+      actContainer.style.display = 'block';
+
+      
+      let newTask = {
+        text: input.value,
+        completed: false
+      };
+
+      tasks.push(newTask);
+
+      createListItem(newTask);
+
+      numberOfItems.value = countRemainingTasks();
     }
-})
-
+  });
 
 
 function createListItem(Task) {
 
+  if (!Task || !Task.text) {
+    return;
+  }
+
   let item = document.createElement('li')
   let label = document.createElement('label')
-
 
   let checkbox = document.createElement('input')
   checkbox.type = 'checkbox'
@@ -47,22 +50,25 @@ function createListItem(Task) {
   })
   checkbox.checked = Task.completed
 
-
   let deleteButton = document.createElement('button')
   deleteButton.type = 'button'
   deleteButton.style.height = '15px'
   deleteButton.textContent = 'X'
 
-  
-  label.append(checkbox, Task.text , deleteButton)
+  label.append(checkbox, Task.text, deleteButton)
   item.append(label)
   list.append(item);
 
   input.value = "";
 }
 
-function showItems(element){
+function countRemainingTasks() {
 
-    element.style.display= 'block';
+  let remainingTasks = tasks.filter(task => task.completed === false)
+
+  return remainingTasks.length
 }
 
+function deletedItems(){
+
+}
